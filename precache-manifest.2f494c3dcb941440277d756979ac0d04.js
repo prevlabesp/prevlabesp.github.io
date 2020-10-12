3907dac7092570a7c1792edeaeba532ee3ef4224
prevlabesp <51543650+prevlabesp@users.noreply.github.com>
2020-10-12 20:33:22 -0300

    First cmmit

---
+++ precache-manifest.2f494c3dcb941440277d756979ac0d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e35eb11b58211a5da7576e70a20130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c30e94fa3a32b6e1c6c6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a03bfd5c2a2dc11f64d5",
+    "url": "/static/js/2.9f4ff6d5.chunk.js"
+  },
+  {
+    "revision": "04d0b5145348ffeeec51d84ca4a63d73",
+    "url": "/static/js/2.9f4ff6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30e94fa3a32b6e1c6c6",
+    "url": "/static/js/main.aa353507.chunk.js"
+  },
+  {
+    "revision": "095080bbad0b54e90ae2",
+    "url": "/static/js/runtime-main.153e7e5e.js"
+  }
+]);
